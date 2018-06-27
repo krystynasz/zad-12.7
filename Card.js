5b8c62848b53306715d8007b399979188a05ad6c
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-06-27 16:28:33 +0200

    Add project files

---
+++ Card.js
@@ -0,0 +1,24 @@
+// KLASA KANBAN CARD
+function Card(description) {
+	var self = this;
+
+	this.id = randomString();
+	this.description = description;
+	this.element = generateTemplate('card-template', {
+		description: this.description,
+		id: this.id
+	}, 'li');
+
+  	this.element.querySelector('.card').addEventListener('click', function (event) {
+    	event.stopPropagation();
+
+    	if (event.target.classList.contains('btn-delete')) {
+      		self.removeCard();
+    	}
+  	});
+}
+Card.prototype = {
+	removeCard: function() {
+		this.element.parentNode.removeChild(this.element);
+    }
+}
