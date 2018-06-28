7afd39bb49909742a086e043cbd8f9091fe8052c
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-06-28 16:34:05 +0200

    Update board.js

--- Board.js
+++ Board.js
@@ -32,40 +32,41 @@ function initSortable(id) {
     var sortable = Sortable.create(el, {
         group: 'kanban',
         sort: true,
+
+
+
+        onEnd: function (/**Event*/evt) {
+            var itemEl = evt.item;  // dragged HTMLElement
+            console.log(itemEl.id);
+            var targetColumn = evt.to;    // target list
+            evt.from;  // previous list
+            evt.oldIndex;  // element's old index within old parent
+            evt.newIndex;  // element's new index within new parent
+            console.log("dragging")
+
+            /*var data = new FormData();
+            data.append('name', itemEl.id);
+            data.append('bootcamp_kanban_column_id',targetColumn.id);
     
-
-
-    onEnd: function (/**Event*/evt) {
-        var itemEl = evt.item;  // dragged HTMLElement
-        console.log(itemEl.id);
-		var targetColumn = evt.to;    // target list
-		evt.from;  // previous list
-		evt.oldIndex;  // element's old index within old parent
-        evt.newIndex;  // element's new index within new parent
-        console.log ("dragging")
-
-        var data = new FormData();
-        data.append('name', itemEl.id);
-        data.append('bootcamp_kanban_column_id',targetColumn.id);
-
-		fetch(baseUrl + '/card/' + itemEl.id, { method: 'PUT', headers: myHeaders, body:data })
-		  .then(function(resp) {
-			return resp.json();
-		  })
-		  .then(function(resp) {
-			console.log(itemEl.id);
-          });
-	},
-    /*var data = new FormData();
-        data.append('name', card.id);
-        data.append('bootcamp_kanban_column_id',id);
-
-		fetch(baseUrl + '/card/' + card.id, { method: 'PUT', headers: myHeaders, body:data })
-		  .then(function(resp) {
-			return resp.json();
-		  })
-		  .then(function(resp) {
-			console.log(card.id);
-          });*/
-        });
-    }
+            fetch(baseUrl + '/card/' + itemEl.id, { method: 'PUT', headers: myHeaders, body:data })
+              .then(function(resp) {
+                return resp.json();
+              })
+              .then(function(resp) {
+                console.log(itemEl.id);
+              });
+        },*/
+            /*var data = new FormData();
+                data.append('name', card.id);
+                data.append('bootcamp_kanban_column_id',id);
+        
+                fetch(baseUrl + '/card/' + card.id, { method: 'PUT', headers: myHeaders, body:data })
+                  .then(function(resp) {
+                    return resp.json();
+                  })
+                  .then(function(resp) {
+                    console.log(card.id);
+                  });*/
+        };
+    };
+}
